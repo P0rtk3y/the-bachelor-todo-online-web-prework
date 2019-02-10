a597c5e11285f1c1699cf0de76a5b6bf9378892c
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-10 00:38:01 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,58 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  #data[season].find{|candidate_info| candidate_info["status"] == "Winner"}["name"].split.first 
+  
+  data[season].each do |contestant_info|
+    if contestant_info["status"] == "Winner"
+      return contestant_info["name"].split(" ")[0]
+    end
+  end 
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  #data.each.find{|contestant_info| contestant_info["occupation"] == occupation}["name"]
+  
+  data.each do |season, contestants_hash|
+    contestants_hash.find do |contestant| 
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+     end 
+    end
+  end 
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, contestants_hash|
+    contestants_hash.each do |contestant|
+      if contestant["hometown"] == hometown 
+        count += 1 
+      end 
+    end 
+  end 
+  count 
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants_hash|
+    contestants_hash.find do |contestant|
+      if contestant["hometown"] == hometown 
+        return contestant["occupation"]
+      end 
+    end 
+  end 
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  num_contestants = data[season].length                          
+  age_sum = 0 
+  age_average = age_sum/num_contestants
+  
+  data[season].each do |contestant_info|
+    age_sum += contestant_info["age"]
+    age_sum 
+    binding.pry
+  end 
+  age_average
 end
