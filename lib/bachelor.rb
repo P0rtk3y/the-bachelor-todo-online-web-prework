78010529f1a2995130e67b052f8153e1ab04481e
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-10 00:43:07 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,9 +49,7 @@ def get_average_age_for_season(data, season)
   age_sum = 0 
   age_average = age_sum/num_contestants
   
-  data[season].each do |contestant_info|
-    age_sum += contestant_info["age"].to_i
-    age_sum 
-  end 
+  age_sum = data[season].collect {|contestant_info| age_sum += contestant_info["age"].to_i} 
   age_average 
+  
 end
